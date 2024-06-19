6d93734e80a4afa8caa27991c71f3c301120ff77
say-1104 <okazaki.sei.a6@elms.hokudai.ac.jp>
2024-06-19 19:05:13 +0900

    notepc

--- abs/abc049c/main.cpp
+++ abs/abc049c/main.cpp
@@ -8,7 +8,7 @@ int main() {
 	cin >> s;
 	string d1 = "dream", d2 = "dreamer", d3 = "erase", d4 = "eraser";
 	while(s.size() > 4) {
-		if(s.substr(s.size()-5) == d1 || s.substr(s.size()-5) == d3) s = s.substr(s.size()-5); 
+		if(s.substr(s.size()-5) == d1 || s.substr(s.size()-5) == d3) s = s.substr(0, s.size()-5); 
 		else if(s.substr(s.size()-6) == d4) s = s.substr(0, s.size()-6);
 		else if(s.substr(s.size()-7) == d2) s = s.substr(0, s.size()-7);
 		else {
--- abs/abc086c/main.cpp
+++ abs/abc086c/main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int main() {
+	int n;
+	vector<int> t = {0}, x = {0}, y = {0};
+	cin >> n;
+	for(int i=0; i<n; i++){
+		int tmp_t, tmp_x, tmp_y;
+		cin >> tmp_t >> tmp_x >> tmp_y;
+		t.push_back(tmp_t);
+		x.push_back(tmp_x);
+		y.push_back(tmp_y);
+	}
+
+	for(int i=0; i<n; i++){
+		int dt = t[i+1] - t[i];
+		int distance = abs(y[i+1] - y[i]) + abs(x[i+1] - x[i]);
+		if(distance > dt) {
+			cout << "No" << endl;
+			return 0;
+		}
+		if(distance % 2 != dt % 2){
+			cout << "No" << endl;
+			return 0;
+		}
+
+	}
+	cout << "Yes" << endl;
 	return 0;
 }
