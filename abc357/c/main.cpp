b4409edd85c01e073c24b51c82fccd709850d46e
say-1104 <okazaki.sei.a6@elms.hokudai.ac.jp>
2024-07-22 14:39:55 +0900

    note

--- abc357/c/main.cpp
+++ abc357/c/main.cpp
@@ -1,7 +1,12 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+vector<int> makesierpinski(vector<vector<int>> int k){
 
+};
 int main() {
+	int n;
+	cin >> n;
+	vector<vector<int>> table(n, vector(n, 1));
 	return 0;
 }
